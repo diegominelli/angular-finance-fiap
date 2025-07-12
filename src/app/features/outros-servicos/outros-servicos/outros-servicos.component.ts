4d26f84e38de43fcb6d0102f51489ffe3bec2e7e
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2025-07-12 19:56:08 -0300

    criacao dos componentes e organizacao do projeto

---
+++ src/app/features/outros-servicos/outros-servicos/outros-servicos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-outros-servicos',
+  standalone: true,
+  imports: [],
+  templateUrl: './outros-servicos.component.html',
+  styleUrl: './outros-servicos.component.scss'
+})
+export class OutrosServicosComponent {
+
+}
---
+++ src/app/features/transferencias/transferencia/transferencias.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transferencia',
+  standalone: true,
+  imports: [],
+  templateUrl: './transferencias.component.html',
+  styleUrl: './transferencias.component.scss',
+})
+export class TransferenciaComponent {}
